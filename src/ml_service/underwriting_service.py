cc661116d608587e8284462efc2456ac3a69af84
boopalan.j <boopalan.j@payoda.com>
2021-07-01 02:27:23 +0530

    updated underwriting_service

--- src/ml_service/underwriting_service.py
+++ src/ml_service/underwriting_service.py
@@ -1,5 +1,7 @@
 import pickle as pickle
 from flask import Flask
+from flask import request
+
 app = Flask(__name__)
 
 # load the saved model
@@ -10,9 +12,19 @@ loaded_model = pickle.load(open(file_name, 'rb'))
 get_risk_factor method
 '''
 @app.route("/getRiskFactor")
-def get_risk_factor(age, is_overweight, has_ped, has_ped2, is_smoking, is_alcoholic, has_undergone_procedure):
+def get_risk_factor():
+    
+    age = int(request.args['age'])
+    is_overweight = int(request.args['is_overweight'])
+    has_ped = int(request.args['has_ped'])
+    has_ped2 = int(request.args['has_ped2'])
+    is_smoking = int(request.args['is_smoking'])
+    is_alcoholic = int(request.args['is_alcoholic'])
+    has_undergone_procedure = int(request.args['has_undergone_procedure'])
+
+
     risk_factor = loaded_model.predict([[age, is_overweight, has_ped, has_ped2, is_smoking, is_alcoholic, has_undergone_procedure]])
-    return risk_factor[0]
+    return dict({"risk_factor": risk_factor[0]})
 
 '''
 ping method
