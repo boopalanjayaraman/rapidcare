6c385feb10ca6b7e4a4b7091bef2b0fdd807fb8a
boopalan.j <boopalan.j@payoda.com>
2021-07-09 14:48:19 +0530

    committing only the keys of configuration so that it can be used and modified. removed the sensitive information of urls, secret keys, access keys, etc.

--- src/server/config/configuration.js
+++ src/server/config/configuration.js
@@ -5,23 +5,39 @@ module.exports = {
     },
     secretOrKey: "secret",
     mailOptions: {
-        host: "smtp.ethereal.email", ////smtp.ethereal.email - for the test email catching service
+        host: "", ////smtp.ethereal.email - for the test email catching service
         port: 587, 
         secure: false, //// true for 465, false for other ports
         auth: {
-            user: "christy10@ethereal.email",
-            pass: "qrDRBuRKksak1qMdzM"
+            user: "", //"christy10@ethereal.email",
+            pass: "",//"qrDRBuRKksak1qMdzM"
         }
     },
     environmentalSettings: {
-        domainBaseUrl: "http://localhost:3000",
+        domainBaseUrl: "http://rapydcare.com",
         currentEnvironment: 'DEV',
-        mailCurrentEnvironmentPrefix : '[DEV]',
-        primaryAdmins: ['aabbbcc@gmail.com'],
+        mailCurrentEnvironmentPrefix : '[DEV] ',
+        primaryAdmins: ['rapydcare.manager@gmail.com'],
     },
     bucketOptions : {
         bucketName : "",
         apiKey : "",
         baseUrl: ""
+    },
+    rapydConfig: {
+        access_key : "",
+        secret_key : "",
+        sandbox_base_url : 'https://sandboxapi.rapyd.net',
+        completionPage_base_url : 'http://rapydcare.com/completePayment',
+        errorPage_base_url : 'http://rapydcare.com/errorPayment',
+        primary_account_wallet : '',
+        service_fee_wallet : ''
+    },
+    UnderwritingAiUrl : "http://127.0.0.1:5001/getRiskFactor",
+    awsConfig: {
+        secretAccessKey : "",
+        accessKeyId : "",
+        region : "",
+        bucketName : ""
     }
   };
